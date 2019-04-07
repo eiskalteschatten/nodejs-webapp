2d3148608a0562be2161f20235ffc429d44d4ef1
Alex <alex@alexseifert.com>
2019-04-07 12:35:25 +0200

    Clean up Docker config

--- config/default.js
+++ config/default.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     tracking: {
         matomo: {
-            url: 'https://url-to-matomo',
+            url: 'https://url-to-matomo/matomo.php',
             siteId: 1
         }
     },
--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@
 module.exports = {
     tracking: {
         matomo: {
-            url: 'https://url-to-matomo',
+            url: 'https://url-to-matomo/matomo.php',
             siteId: 1
         }
     }
