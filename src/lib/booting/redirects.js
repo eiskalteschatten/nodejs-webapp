4a35e5b485008dd40d6d0c0ad02acab4aca76dd9
Alex <alex@alexseifert.com>
2019-04-21 16:44:27 +0200

    Redirects now work regardless of whether the url as a trailing slash or not

--- src/lib/booting/redirects.js
+++ src/lib/booting/redirects.js
@@ -7,13 +7,16 @@ const { parseRoute } = require('../helper');
 
 
 module.exports = (req, res, next) => {
-    const originalUrl = '/' + parseRoute(req.originalUrl);
-    const newUrl = redirects[originalUrl];
+    const originalUrl = '/' + parseRoute(req.originalUrl.replace(/\/$/, ''));
+    const regexString = `^${originalUrl}/?$`;
+    const regex = new RegExp(regexString, 'g');
 
-    if (newUrl) {
-        const redirectUrl = '/' + req.lang + newUrl;
-        res.redirect(301, redirectUrl);
-        return;
+    for (const redirectedUrl in redirects) {
+        if (regex.test(redirectedUrl)) {
+            const redirectUrl = '/' + req.lang + redirects[redirectedUrl];
+            res.redirect(301, redirectUrl);
+            return;
+        }
     }
 
     next();
